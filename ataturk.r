21bf74a28e8cc11366cc82d83dfd67db1a328fd2
Yasin KUTUK <yasinkutuk@gmail.com>
2021-10-29 13:47:56 +0300

    Add files via upload

--- ataturk.r
+++ ataturk.r
@@ -26,14 +26,36 @@
 
 #Hamiş:Lütfen lokasyon bilgilerini kendinize göre düzeltiniz.
 
-path <- '/media/DRIVE/Dropbox/_My_Research/Ataturk/'
-
-ata <- data.frame(read.csv(file='/media/DRIVE/Dropbox/_My_Research/Ataturk/ataturk.csv', sep=';'))
+path <- '/media/DRIVE/Dropbox/_My_Research/Ataturk-Data/'
 
 
-wi <- 750 
+ata <- data.frame(read.csv(file='/media/DRIVE/Dropbox/_My_Research/Ataturk-Data/ata.csv', sep=';'))
+
+wi <- 800
 he <- 1200  
 
 png(paste0(path,'01ataturk.png'), width = wi, height = he)
-plot(ata$turk, xlab = 'Mustafa Kemal ATATÜRK', ylab='Gazi')
+plot(ata$q3,ata$q4, xlab = 'Mustafa Kemal ATATÜRK', ylab='Gazi')
 dev.off()
+
+
+turkey <- data.frame(read.csv(file='/media/DRIVE/Dropbox/_My_Research/Ataturk-Data/turkey.csv', sep=';'))
+
+wi <- 1200
+he <- 800  
+
+png(paste0(path,'02turkey.png'), width = wi, height = he)
+plot(turkey$q1,turkey$q2, xlab = 'Türkiye', ylab='BW Haritasi')
+dev.off()
+
+
+tr <- data.frame(ata,turkey)
+colnames(tr) <- c('atax','atay','trx','try')
+tr$ataturk <- (ata$q3+ata$q4)/2
+tr$turkiye <- (turkey$q1+turkey$q2)/2
+
+
+summary(tr)
+summary(lm(turkiye~ataturk, data = tr))
+
+write.csv(tr,file='/media/DRIVE/Dropbox/_My_Research/Ataturk-Data/atatr.csv')
